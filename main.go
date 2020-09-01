04e194280c46f7cbaa7c87be9f190506d1867b96
Tillo <tillo.bosshart@gmail.com>
2020-09-01 11:48:53 +0200

    pdfgeneration added

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 	"text/template"
 
@@ -57,20 +58,55 @@ func output(data, templ, out string) {
 	f.Close()
 }
 
+func runcmd(cmd string, file string) {
+	c := exec.Command(cmd, file)
+	st, err := c.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(st))
+}
+
+func cleanup() {
+	remFile("cv.aux")
+	remFile("cv.log")
+	remFile("cv.out")
+	remFile("output/letter.aux")
+}
+
+func remFile(path string) {
+	err := os.Remove(path)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
 func main() {
 	var templ, data, out string
-	var letter bool
+	var letter, pdfl, xel bool
 	var ltempl, ldata string
 	flag.StringVar(&templ, "t", "templates/template.tex", "Path to cv template")
 	flag.StringVar(&data, "i", "input/cv.yaml", "Path to cv data yaml file")
 	flag.StringVar(&out, "o", "output/cv.tex", "Path to output file")
 	flag.BoolVar(&letter, "L", false, "Compile a cover letter, the template should have it enabled too")
 	flag.StringVar(&ltempl, "c", "templates/letter_template.tex", "Path to letter template")
-	flag.StringVar(&ldata, "l", "input/letter.yaml", "Path to lette data yaml file")
+	flag.StringVar(&ldata, "l", "input/letter.yaml", "Path to letter data yaml file")
+	flag.BoolVar(&pdfl, "P", false, "Genderate a pdf using pdflatex")
+	flag.BoolVar(&xel, "X", false, "Genderate a pdf using xelatex")
+
 	flag.Parse()
 
 	if letter {
 		output(ldata, ltempl, "output/letter.tex")
 	}
 	output(data, templ, out)
+	if pdfl {
+		runcmd("pdflatex", out)
+		cleanup()
+	}
+	if xel {
+		runcmd("xelatex", out)
+		cleanup()
+	}
 }
