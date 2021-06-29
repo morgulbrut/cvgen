cc00ac28ce3e36198608cad26632a2c4e0223d19
Tillo <tillo.bosshart@gmail.com>
2021-06-29 17:00:26 +0200

    made the section titles part of the input yaml

--- cv/cv.go
+++ cv/cv.go
@@ -8,127 +8,151 @@ import (
 )
 
 type CV struct {
-	Recipient struct {
-		Name     string `yaml:"name"`
-		Company  string `yaml:"company"`
-		Prename  string `yaml:"prename"`
-		Address  string `yaml:"address"`
-		Postcode string `yaml:"postcode"`
-		City     string `yaml:"city"`
-		Country  string `yaml:"country"`
-	} `yaml:"recipient"`
-	Letter struct {
-		Opening string   `yaml:"opening"`
-		Closing string   `yaml:"closing"`
-		Text    []string `yaml:"text"`
-		Date    string   `yaml:"date"`
-	} `yaml:"letter"`
-
 	Personalinfo struct {
-		Name     string `yaml:"name"`
-		Prename  string `yaml:"prename"`
-		Address  string `yaml:"address"`
-		Postcode string `yaml:"postcode"`
-		City     string `yaml:"city"`
-		Country  string `yaml:"country"`
-		Mobile   string `yaml:"mobile"`
-		Phone    string `yaml:"phone"`
-		Fax      string `yaml:"fax"`
-		Title    string `yaml:"title"`
-		Email    string `yaml:"email"`
-		Homepage string `yaml:"homepage"`
-		Linkedin string `yaml:"linkedin"`
-		Twitter  string `yaml:"twitter"`
-		Github   string `yaml:"github"`
-		Gitlab   string `yaml:"gitlab"`
-		Quote    string `yaml:"quote"`
-		Photo    string `yaml:"photo"`
+		Prename  string      `yaml:"prename"`
+		Name     string      `yaml:"name"`
+		Address  string      `yaml:"address"`
+		Postcode string      `yaml:"postcode"`
+		City     string      `yaml:"city"`
+		Country  string      `yaml:"country"`
+		Mobile   string      `yaml:"mobile"`
+		Phone    string      `yaml:"phone"`
+		Fax      string      `yaml:"fax"`
+		Email    string      `yaml:"email"`
+		Homepage string      `yaml:"homepage"`
+		Linkedin string      `yaml:"linkedin"`
+		Twitter  string      `yaml:"twitter"`
+		Github   string      `yaml:"github"`
+		Gitlab   string      `yaml:"gitlab"`
+		Quote    string      `yaml:"quote"`
+		Photo    interface{} `yaml:"photo"`
+		Title    string      `yaml:"title"`
 	} `yaml:"personalinfo"`
-	Experience []struct {
-		Company     string   `yaml:"company"`
-		Role        string   `yaml:"role"`
-		City        string   `yaml:"city"`
-		Date        string   `yaml:"date"`
-		Description string   `yaml:"description"`
-		Tasks       []string `yaml:"tasks"`
-	} `yaml:"experience"`
-	Projects []struct {
-		Title       string   `yaml:"title"`
-		Context     string   `yaml:"context"`
-		City        string   `yaml:"city"`
-		Date        string   `yaml:"date"`
-		Description string   `yaml:"description"`
-		Tasks       []string `yaml:"tasks"`
-	} `yaml:"projects"`
-	Education []struct {
-		School      string `yaml:"school"`
-		Subject     string `yaml:"subject"`
-		City        string `yaml:"city"`
-		Date        string `yaml:"date"`
-		Description string `yaml:"description"`
-	} `yaml:"education"`
-	Computerskills []struct {
-		Group string `yaml:"group"`
-		Skill string `yaml:"skill"`
-	} `yaml:"computerskills"`
-	Testimonials []struct {
-		Name  string `yaml:"name"`
-		Quote string `yaml:"quote"`
-	} `yaml:"testimonials"`
 	Freetext struct {
 		Begin string `yaml:"begin"`
 		End   string `yaml:"end"`
 	} `yaml:"freetext"`
-	Hackathons []struct {
-		Name        string `yaml:"name"`
-		Role        string `yaml:"role"`
-		City        string `yaml:"city"`
-		Date        string `yaml:"date"`
-		Description string `yaml:"description"`
-	} `yaml:"hackathons"`
-	Talks []struct {
-		Title       string `yaml:"title"`
-		Coautors    string `yaml:"coautors"`
-		Conference  string `yaml:"conference"`
-		Date        string `yaml:"date"`
-		Description string `yaml:"description"`
-	} `yaml:"talks"`
-	Publications []struct {
-		Title       string `yaml:"title"`
-		Coautors    string `yaml:"coautors"`
-		Context     string `yaml:"context"`
-		Date        string `yaml:"date"`
-		Description string `yaml:"description"`
-	} `yaml:"publications"`
-	Certificates []struct {
-		Type string `yaml:"type"`
-		Name string `yaml:"name"`
-	} `yaml:"certificates"`
-	Interests []struct {
-		Group       string `yaml:"group"`
-		Description string `yaml:"description"`
-	} `yaml:"interests"`
-	Languages []struct {
-		Language string `yaml:"language"`
-		Level    string `yaml:"level"`
+	Testimonials []struct {
+		Name  string `yaml:"name"`
+		Quote string `yaml:"quote"`
+	} `yaml:"testimonials"`
+	Experience struct {
+		Title string `yaml:"title"`
+		Jobs  []struct {
+			Company     string   `yaml:"company"`
+			Role        string   `yaml:"role"`
+			City        string   `yaml:"city"`
+			Date        string   `yaml:"date"`
+			Description string   `yaml:"description"`
+			Tasks       []string `yaml:"tasks"`
+		} `yaml:"jobs"`
+	} `yaml:"experience"`
+	Education struct {
+		Title   string `yaml:"title"`
+		Schools []struct {
+			School      string `yaml:"school"`
+			Subject     string `yaml:"subject"`
+			City        string `yaml:"city"`
+			Date        string `yaml:"date"`
+			Description string `yaml:"description"`
+		} `yaml:"schools"`
+	} `yaml:"education"`
+	Languages struct {
+		Title     string `yaml:"title"`
+		Languages []struct {
+			Language string `yaml:"language"`
+			Level    string `yaml:"level"`
+		} `yaml:"languages"`
 	} `yaml:"languages"`
-
-	// moderncv settings
-	Settings struct {
-		Date     string `yaml:"date"`
-		Cvstyle  string `yaml:"cvstyle"`
-		Cvcolor  string `yaml:"cvcolor"`
-		Mainfont string `yaml:"mainfont"`
-		Sansfont string `yaml:"sansfont"`
-	} `yaml:"settings"`
-
-	// developercv settings
+	Computerskills struct {
+		Title          string `yaml:"title"`
+		Computerskills []struct {
+			Group string `yaml:"group"`
+			Skill string `yaml:"skill"`
+		} `yaml:"computerskills"`
+	} `yaml:"computerskills"`
+	Hackathons struct {
+		Title      string `yaml:"title"`
+		Hackathons []struct {
+			Name        string `yaml:"name"`
+			Role        string `yaml:"role"`
+			City        string `yaml:"city"`
+			Date        string `yaml:"date"`
+			Description string `yaml:"description"`
+		} `yaml:"hackathons"`
+	} `yaml:"hackathons"`
+	Projects struct {
+		Title    string `yaml:"title"`
+		Projects []struct {
+			Title       string   `yaml:"title"`
+			City        string   `yaml:"city"`
+			Context     string   `yaml:"context"`
+			Date        string   `yaml:"date"`
+			Description string   `yaml:"description"`
+			Tasks       []string `yaml:"tasks"`
+		} `yaml:"projects"`
+	} `yaml:"projects"`
+	Talks struct {
+		Title string `yaml:"title"`
+		Talks []struct {
+			Title       string `yaml:"title"`
+			Coautors    string `yaml:"coautors"`
+			Conference  string `yaml:"conference"`
+			Date        string `yaml:"date"`
+			Description string `yaml:"description"`
+		} `yaml:"talks"`
+	} `yaml:"talks"`
+	Publications struct {
+		Title        string `yaml:"title"`
+		Publications []struct {
+			Title       string `yaml:"title"`
+			Coautors    string `yaml:"coautors"`
+			Context     string `yaml:"context"`
+			Date        string `yaml:"date"`
+			Description string `yaml:"description"`
+		} `yaml:"publications"`
+	} `yaml:"publications"`
+	Certificates struct {
+		Title        string `yaml:"title"`
+		Certificates []struct {
+			Type string `yaml:"Type"`
+			Name string `yaml:"Name"`
+		} `yaml:"certificates"`
+	} `yaml:"certificates"`
+	Interests struct {
+		Title     string `yaml:"title"`
+		Interests []struct {
+			Group       string `yaml:"group"`
+			Description string `yaml:"description"`
+		} `yaml:"interests"`
+	} `yaml:"interests"`
 	Skills []struct {
 		Name  string `yaml:"name"`
-		Level string `yaml:"level"`
+		Level int    `yaml:"level"`
 	} `yaml:"skills"`
 	Hobbies   string `yaml:"hobbies"`
 	Nonprofit string `yaml:"nonprofit"`
+	Settings  struct {
+		Date     string      `yaml:"date"`
+		Cvstyle  string      `yaml:"cvstyle"`
+		Cvcolor  string      `yaml:"cvcolor"`
+		Mainfont interface{} `yaml:"mainfont"`
+		Sansfont interface{} `yaml:"sansfont"`
+	} `yaml:"settings"`
+	Recipient struct {
+		Name     string      `yaml:"name"`
+		Company  string      `yaml:"company"`
+		Prename  interface{} `yaml:"prename"`
+		Address  string      `yaml:"address"`
+		Postcode int         `yaml:"postcode"`
+		City     string      `yaml:"city"`
+	} `yaml:"recipient"`
+	Letter struct {
+		Date    string   `yaml:"date"`
+		Opening string   `yaml:"opening"`
+		Closing string   `yaml:"closing"`
+		Text    []string `yaml:"text"`
+	} `yaml:"letter"`
 }
 
 func (cv *CV) Read(f string) *CV {
